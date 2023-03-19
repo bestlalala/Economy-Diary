d54bdfb15223e52737ab40e094ed4f408de1151a
yeonsu <bestdustn@sookmyung.ac.kr>
2023-03-19 17:15:46 +0900

    backend initial commit

---
+++ economy_diary/src/main/java/project/economy_diary/EconomyDiaryApplication.java
@@ -0,0 +1,13 @@
+package project.economy_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomyDiaryApplication.class, args);
+	}
+
+}
---
+++ economy_diary/src/test/java/project/economy_diary/EconomyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.economy_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomyDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
