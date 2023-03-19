aa8aa9336275126ad6622fc00310d2d7c1d87383
yeonsu <bestdustn@sookmyung.ac.kr>
2023-03-19 16:41:28 +0900

    back/initial_commit

---
+++ ecnomy_diary/src/main/java/project/economy/ecnomy_diary/EcnomyDiaryApplication.java
@@ -0,0 +1,13 @@
+package project.economy.ecnomy_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcnomyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcnomyDiaryApplication.class, args);
+	}
+
+}
---
+++ ecnomy_diary/src/test/java/project/economy/ecnomy_diary/EcnomyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.economy.ecnomy_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcnomyDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
